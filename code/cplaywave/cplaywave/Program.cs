3fb2ad208780e3da3f1b50800850a3315c06b621
朱威宇 <673143864@qq.com>
2015-07-23 16:20:20 +0800

    c# 7/23

---
+++ code/cplaywave/cplaywave/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace cplaywave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playwave temp = new Playwave();
+            temp.play(5);
+            Thread.Sleep(300);
+            temp.play(5);
+            Thread.Sleep(300);
+            temp.play(6); 
+            Thread.Sleep(300);
+            temp.play(7); 
+            Thread.Sleep(300);
+            temp.play(7);
+            Thread.Sleep(300);
+            temp.play(6); 
+            Thread.Sleep(300);
+            temp.play(5);
+            Thread.Sleep(300);
+            temp.play(4);
+            Thread.Sleep(800);
+
+            temp.play(5);
+            Thread.Sleep(300);
+            temp.play(5);
+            Thread.Sleep(300);
+            temp.play(6);
+            Thread.Sleep(300);
+            temp.play(7);
+            Thread.Sleep(300);
+            temp.play(7);
+            Thread.Sleep(300);
+            temp.play(6);
+            Thread.Sleep(300);
+            temp.play(5);
+            Thread.Sleep(300);
+            temp.play(4);
+
+            Thread.Sleep(800);
+            temp.play(102);
+            Thread.Sleep(300);
+            temp.play(102);
+            Thread.Sleep(300);
+            temp.play(101);
+            Thread.Sleep(300);
+            temp.play(102);
+            Thread.Sleep(300);
+            temp.play(101);
+            Thread.Sleep(300);
+            temp.play(103);
+            Thread.Sleep(300);
+            temp.play(102);
+            Thread.Sleep(300);
+            temp.play(100);
+
+            Console.Read();
+        }
+    }
+}
